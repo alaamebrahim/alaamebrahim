ff5121e0c86804a00afdeb58176a6db264be7dc5
alaa mohammed <alaamebrahim91@gmail.com>
2019-05-04 17:58:37 +0200

    add new site

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,23 @@
 jQuery(document).ready(function(){
     "use strict";
-// Preloader Js
-jQuery(window).on('load', function() {
-    jQuery("#status").fadeOut();
-    jQuery("#preloader").delay(200).fadeOut("slow");
-});
+
+    // Preloader Js
+    jQuery(window).on('load', function() {
+        jQuery("#status").fadeOut();
+        jQuery("#preloader").delay(200).fadeOut("slow");
+    });
+
+    //Shehada gallery
+    $("#shehada_site").click(function() {
+        $.fancybox.open([
+            {
+            'src':             './images/shehada/shehada1.png',
+            'padding'			: 0,
+            'transitionIn'		: 'none',
+            'transitionOut'		: 'none',
+            'type'              : 'image',
+            'changeFade'        : 0
+        }]);
+    });
 
 });
