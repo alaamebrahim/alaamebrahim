a6f4ca9f9f565529038d3ef89e82ee59373712ca
alaa mohammed <alaamebrahim91@gmail.com>
2019-05-04 18:08:44 +0200

    finish adding certificates generator website

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,23 +1,65 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
     "use strict";
 
     // Preloader Js
-    jQuery(window).on('load', function() {
+    jQuery(window).on('load', function () {
         jQuery("#status").fadeOut();
         jQuery("#preloader").delay(200).fadeOut("slow");
     });
 
     //Shehada gallery
-    $("#shehada_site").click(function() {
+    $("#shehada_site").click(function () {
+        let s_url = window.location.protocol + '//' + window.location.hostname;
         $.fancybox.open([
             {
-            'src':             './images/shehada/shehada1.png',
-            'padding'			: 0,
-            'transitionIn'		: 'none',
-            'transitionOut'		: 'none',
-            'type'              : 'image',
-            'changeFade'        : 0
-        }]);
+                'src': s_url + '/assets/images/shehada/shehada1.png',
+                'padding': 10,
+                'transitionIn': 'none',
+                'transitionOut': 'none',
+                'type': 'image',
+                'changeFade': 0
+            },
+            {
+                'src': s_url + '/assets/images/shehada/shehada11.png',
+                'padding': 10,
+                'transitionIn': 'none',
+                'transitionOut': 'none',
+                'type': 'image',
+                'changeFade': 0
+            },
+            {
+                'src': s_url + '/assets/images/shehada/shehada2.png',
+                'padding': 10,
+                'transitionIn': 'none',
+                'transitionOut': 'none',
+                'type': 'image',
+                'changeFade': 0
+            },
+            {
+                'src': s_url + '/assets/images/shehada/shehada21.png',
+                'padding': 10,
+                'transitionIn': 'none',
+                'transitionOut': 'none',
+                'type': 'image',
+                'changeFade': 0
+            },
+            {
+                'src': s_url + '/assets/images/shehada/shehada3.png',
+                'padding': 10,
+                'transitionIn': 'none',
+                'transitionOut': 'none',
+                'type': 'image',
+                'changeFade': 0
+            },
+            {
+                'src': s_url + '/assets/images/shehada/shehada31.png',
+                'padding': 10,
+                'transitionIn': 'none',
+                'transitionOut': 'none',
+                'type': 'image',
+                'changeFade': 0
+            },
+        ]);
     });
 
 });
