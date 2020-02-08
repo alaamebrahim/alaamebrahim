797dddb426a8ff658f057e12146c8cacd26b4b56
Alaa m ebrahim <alaamebrahim91@gmail.com>
2020-02-08 13:25:41 +0200

    redesigned my website with tailwind

---
+++ webpack.mix.js
@@ -0,0 +1,13 @@
+const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
+const tailwindcss = require('tailwindcss');
+
+mix
+    .sass('assets/scss/design.scss', 'assets/css/design.css')
+    .scripts([
+        'assets/js/custom.js',
+    ], 'assets/js/scripts.min.js')
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss('./tailwind.config.js')],
+    }).purgeCss({});
